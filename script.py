73690a4b27bc7d39236ced0405320964764f7509
dy6P <peronidylan06@gmail.com>
2026-03-16 19:09:49 +0100

    Commit.

--- script.py
+++ script.py
@@ -3,63 +3,86 @@ import json
 
 ENDPOINT = "https://query.wikidata.org/sparql"
 sparql = SPARQLWrapper(ENDPOINT)
+ACTEURS = ["Leonardo DiCaprio", "Brad Pitt", "Tom Hanks", "Meryl Streep"]
 
-ACTORS = ["Leonardo DiCaprio", "Brad Pitt", "Tom Hanks"]
-
-def actor_query(actor_name):
-    return f"""
-    SELECT ?film ?filmLabel ?date ?genreLabel ?director ?directorLabel ?actor ?actorLabel ?birthDate ?role
+def recuperer_tout(nom_acteur):
+    requete = f"""
+    SELECT DISTINCT ?film ?filmLabel ?date ?genreLabel ?realisateur ?realisateurLabel ?realisateurNaissance ?membreCasting ?membreCastingLabel ?membreNaissance ?roleLabel
     WHERE {{
-      ?actor rdfs:label "{actor_name}"@en .
-      ?film wdt:P161 ?actor .
-
-      OPTIONAL {{ ?film wdt:P577 ?date }}
-      OPTIONAL {{ ?film wdt:P136 ?genre }}
-      OPTIONAL {{ ?film wdt:P57 ?director }}
-      OPTIONAL {{ ?actor wdt:P569 ?birthDate }}
-
-      ?film wdt:P161 ?actor .
-      ?actor rdfs:label ?actorLabel .
-      ?film rdfs:label ?filmLabel .
-
-      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+        ?acteur rdfs:label "{nom_acteur}"@en .
+        {{ SELECT DISTINCT ?film WHERE {{
+            ?acteur rdfs:label "{nom_acteur}"@en .
+            ?film wdt:P161 ?acteur ;
+                wdt:P166 [] .
+        }} LIMIT 3 }}
+        OPTIONAL {{ ?film wdt:P577 ?date }}
+        OPTIONAL {{ ?film wdt:P136 ?genre }}
+        OPTIONAL {{
+            ?film wdt:P57 ?realisateur .
+            OPTIONAL {{ ?realisateur wdt:P569 ?realisateurNaissance }}
+        }}
+        OPTIONAL {{
+            ?film p:P161 ?declaration .
+            ?declaration ps:P161 ?membreCasting .
+            OPTIONAL {{ ?membreCasting wdt:P569 ?membreNaissance }}
+            OPTIONAL {{ ?declaration pq:P453 ?role }}
+        }}
+        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "fr,en". }}
     }}
-    LIMIT 3
     """
-
-def sparql_query(query):
-    sparql.setQuery(query)
+    sparql.setQuery(requete)
     sparql.setReturnFormat(JSON)
-    return sparql.query().convert()
+    return sparql.query().convert()["results"]["bindings"]
+
+
+def construire_documents(lignes):
+    films = {}
+    for ligne in lignes:
+        id_film = ligne["film"]["value"].split("/")[-1]
+        if id_film not in films:
+            films[id_film] = {
+                "wikidata_id": id_film,
+                "titre": ligne.get("filmLabel", {}).get("value", ""),
+                "annee": ligne.get("date", {}).get("value", "")[:4],
+                "genres": [],
+                "realisateur": {},
+                "casting": []
+            }
+            if ligne.get("realisateur"):
+                films[id_film]["realisateur"] = {
+                    "wikidata_id": ligne["realisateur"]["value"].split("/")[-1],
+                    "nom": ligne.get("realisateurLabel", {}).get("value", ""),
+                    "dateNaissance": ligne.get("realisateurNaissance", {}).get("value", "")[:10]
+                }
+        film = films[id_film]
+        genre = ligne.get("genreLabel", {}).get("value")
+        if genre and genre not in film["genres"]:
+            film["genres"].append(genre)
+        membre = ligne.get("membreCasting", {}).get("value")
+        if membre:
+            id_membre = membre.split("/")[-1]
+            ids_casting = [m["wikidata_id"] for m in film["casting"]]
+            if id_membre not in ids_casting:
+                film["casting"].append({
+                    "wikidata_id": id_membre,
+                    "nom": ligne.get("membreCastingLabel", {}).get("value", ""),
+                    "dateNaissance": ligne.get("membreNaissance", {}).get("value", "")[:10],
+                    "role": ligne.get("roleLabel", {}).get("value", "")
+                })
+    return films
+
 
 def main():
-    films = []
-    for actor in ACTORS:
-        query = actor_query(actor)
-        data = sparql_query(query)
-        for film_data in data["results"]["bindings"]:
-            film = {
-                "wikidata_id": film_data["film"]["value"].split("/")[-1],
-                "title": film_data.get("filmLabel", {}).get("value", "Unknown Title"),
-                "year": film_data.get("date", {}).get("value", "")[:4],
-                "genres": [film_data.get("genreLabel", {}).get("value")] if film_data.get("genreLabel") else [],
-                "director": {
-                    "name": film_data.get("directorLabel", {}).get("value", "Unknown Director"),
-                    "birthDate": "",
-                    "wikidata_id": film_data.get("director", {}).get("value", "").split("/")[-1]
-                },
-                "cast": [
-                    {
-                        "name": actor,
-                        "birthDate": "",
-                        "role": "",
-                        "wikidata_id": ""
-                    }
-                ]
-            }
-            films.append(film)
-    with open("films.json", "w") as f:
-        json.dump(films, f, indent=2)
+    tous_les_films = {}
+    for acteur in ACTEURS:
+        lignes = recuperer_tout(acteur)
+        films = construire_documents(lignes)
+        for id_film, doc in films.items():
+            if id_film not in tous_les_films:
+                tous_les_films[id_film] = doc
+    with open("films.json", "w", encoding="utf-8") as f:
+        json.dump(list(tous_les_films.values()), f, indent=True, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     main()
