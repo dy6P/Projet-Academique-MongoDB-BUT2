3cc1ac1af3243b06067ba61e4f60c6220133c373
22401673 <peronidylan06@gmail.com>
2026-03-16 11:11:06 +0100

    Commit.

--- script.py
+++ script.py
@@ -4,26 +4,23 @@ import json
 ENDPOINT = "https://query.wikidata.org/sparql"
 sparql = SPARQLWrapper(ENDPOINT)
 
-ACTORS = [
-    "Leonardo DiCaprio",
-    "Brad Pitt",
-    "Tom Hanks"
-]
+ACTORS = ["Leonardo DiCaprio", "Brad Pitt", "Tom Hanks"]
 
 def actor_query(actor_name):
-    query = f"""
-    SELECT ?id ?title ?date ?genre ?director
+    return f"""
+    SELECT ?film ?filmLabel ?date ?genreLabel ?director ?directorLabel
     WHERE {{
-        ?actor rdfs:label "{actor_name}"@en .
-        ?id wdt:P161 ?actor .
-        OPTIONAL {{ ?id rdfs:label ?title FILTER (lang(?title)="en") }}
-        OPTIONAL {{ ?id wdt:P577 ?date }}
-        OPTIONAL {{ ?id wdt:P136 ?genre }}
-        OPTIONAL {{ ?id wdt:P57 ?director }}
+      ?actor rdfs:label "{actor_name}"@en .
+      ?film wdt:P161 ?actor .
+
+      OPTIONAL {{ ?film wdt:P577 ?date }}
+      OPTIONAL {{ ?film wdt:P136 ?genre }}
+      OPTIONAL {{ ?film wdt:P57 ?director }}
+
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
     }}
     LIMIT 3
     """
-    return query
 
 def sparql_query(query):
     sparql.setQuery(query)
@@ -37,11 +34,26 @@ def main():
         data = sparql_query(query)
         for film_data in data["results"]["bindings"]:
             film = {
-                "wikidata_id": film_data["id"]["value"].split("/")[-1],
-                "title": film_data.get("title", {}).get("value"),
+                "wikidata_id": film_data["film"]["value"].split("/")[-1],
+                "title": film_data.get("filmLabel", {}).get("value"),
                 "year": film_data.get("date", {}).get("value", "")[:4],
-                "genre": film_data.get("genre", {}).get("value"),
-                "director": film_data.get("director", {}).get("value")
+                "genres": [
+                    film_data.get("genreLabel", {}).get("value")
+                ] if "genreLabel" in film_data else [],
+                "director": {
+                    "name": film_data.get("directorLabel", {}).get("value"),
+                    "birthDate": "",
+                    "wikidata_id": film_data["director"]["value"].split("/")[-1]
+                    if "director" in film_data else ""
+                },
+                "cast": [
+                    {
+                        "name": actor,
+                        "birthDate": "",
+                        "role": "",
+                        "wikidata_id": ""
+                    }
+                ]
             }
             films.append(film)
     with open("films.json", "w") as f:
